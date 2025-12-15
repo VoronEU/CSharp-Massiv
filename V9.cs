c80b0dc75c42f1420985502bbd46012cfb0f62bd
VoronEU <93764594+VoronEU@users.noreply.github.com>
2025-12-15 22:05:52 +0600

    Add program to count common elements in two arrays

---
+++ V9.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = int.Parse(Console.ReadLine());
+
+        int m = int.Parse(Console.ReadLine());
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) b[i] = int.Parse(Console.ReadLine());
+
+        int count = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (a[i] == b[j])
+                {
+                    count++;
+                    break;
+                }
+
+        Console.WriteLine(count);
+    }
+}
